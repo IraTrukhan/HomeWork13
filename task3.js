2f315b8787785a3d2186864b206025de9e55bd30
IraTrukhan <iryna.viaz@gmail.com>
2021-10-26 21:01:32 +0300

    add task3

---
+++ task3.js
@@ -0,0 +1,12 @@
+function User(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.hello = function() {
+        alert(`Hello, my firstName is ${this.firstname}, lastName is ${this.lastname}. I am very exited to learn JavaScript!`);
+      };
+  }
+  
+  let firstname = prompt('Enter your firstname:', 'Valik');
+  let lastName = prompt('Enter your lastname:', 'Trukhan');
+  const obj = new User(firstname, lastName);
+  obj.hello();
