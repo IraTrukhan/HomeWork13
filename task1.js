2688f0e5d56fdc57aa4e325d7c3107bdccdb1d84
IraTrukhan <iryna.viaz@gmail.com>
2021-10-26 20:49:59 +0300

    add task1

---
+++ task1.js
@@ -0,0 +1,29 @@
+function returnNumberName(currentNumber){
+    switch (+currentNumber) {
+        case 0:
+          return('zero');
+        case 1:
+          return('one');
+        case 2:
+            return('two');
+        case 3:
+          return('three');
+        case 4:
+          return('four');
+        case 5:
+          return('five');
+        case 6:
+          return('six');
+        case 7:
+          return('seven');
+        case 8:
+          return('eight');
+        case 9:
+          return('nine');
+        default:
+          return false;
+      }
+}
+let changeNumber = returnNumberName(prompt('Enter number', 0));
+console.log(changeNumber);
+changeNumber ? alert (changeNumber) : alert ('Error');
